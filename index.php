e5c00c906a106132185cf08ce498ad5c75440605
swaraliRaut <raut.swarali3@gmail.com>
2014-11-01 16:59:09 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html><body>
+<form action="#">
+Enter a: <input type="text" name="a"/>
+Enter b: <input type="text" name="b"/>
+<input type="submit"/>
+</form>
+
+<?php
+
+$first_number = a;
+$second_number = b;
+$sum_total = $a + $b;
+
+echo $sum_total;
+
+?>
+
+</body>
+</html>
